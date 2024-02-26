5f98e0a7790a4e03415a1bda4041722b00882b0b
하민철 <minchulha0703@gmail.com>
2024-02-26 02:29:55 -0500

    Fetch coin data from external API with axios

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -38,6 +40,12 @@ const Title = styled.h1`
     color: ${props => props.theme.accentColor};
 `;
 
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+    padding-top: 20px;
+`;
+
 const coins = [
     {
         id: "btc-bitcoin",
@@ -68,19 +76,45 @@ const coins = [
         },
 ]
 
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
+
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+    const getCoins = async() => {
+        const response = await axios("https://api.coinpaprika.com/v1/coins");
+        setCoins(response.data.slice(0, 100));
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getCoins();
+    });
+
     return (
         <Container>
             <Header>
                 <Title>Coin</Title>
             </Header>
-            <CoinsList>
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <CoinsList>
                 {coins.map((coin) => (
                     <Coin key={coin.id}>
                         <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
                     </Coin>
                 ))}
             </CoinsList>
+            )}
         </Container>
     );
 }
