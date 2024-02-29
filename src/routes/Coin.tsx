95c8247b652267b3188cbd25dd8477d20720db76
하민철 <minchulha0703@gmail.com>
2024-02-28 23:58:25 -0500

    Add fetching coin info and price

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -39,6 +39,20 @@ function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams() as unknown as RouteParams;
     const { state } = useLocation() as LocationState;
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, []);
     return (
         <Container>
             <Header>
