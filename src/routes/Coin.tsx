640934ab8c865a5e447911a32e09fe27d44897b0
하민철 <minchulha0703@gmail.com>
2024-03-04 23:58:45 -0500

    Add InfoData interface

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -36,6 +36,37 @@ interface LocationState {
     }
 }
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    tags: string[];
+    team: string[];
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    links: any;
+    links_extended: any;
+    whitepaper: any;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData {
+
+}
+
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams() as unknown as RouteParams;
