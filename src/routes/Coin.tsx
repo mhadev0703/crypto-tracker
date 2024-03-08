0e5fff048fe9bf9d3c4a510725a7373035f3ec48
하민철 <minchulha0703@gmail.com>
2024-03-07 23:54:26 -0500

    Modify price data interface

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -73,7 +73,27 @@ interface PriceData {
     beta_value: number;
     first_data_at: string;
     last_updated: string;
-    quotes: any;
+    quotes: {
+        USD: {
+          ath_date: string;
+          ath_price: number;
+          market_cap: number;
+          market_cap_change_24h: number;
+          percent_change_1h: number;
+          percent_change_1y: number;
+          percent_change_6h: number;
+          percent_change_7d: number;
+          percent_change_12h: number;
+          percent_change_15m: number;
+          percent_change_24h: number;
+          percent_change_30d: number;
+          percent_change_30m: number;
+          percent_from_price_ath: number;
+          price: number;
+          volume_24h: number;
+          volume_24h_change_24h: number;
+        };
+    }
 }
 
 function Coin() {
