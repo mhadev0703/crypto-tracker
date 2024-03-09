2054834219fdef26156f98f99baa64ea0bf4cd8e
하민철 <minchulha0703@gmail.com>
2024-03-08 23:58:31 -0500

    Partial modification of the router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
+//import Chart from "routes/Chart";
+//import Price from "routes/Price";
 
 function Router() {
     return (
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -108,11 +108,9 @@ function Coin() {
             try {
                 const infoResponse = await axios.get(`https://api.coinpaprika.com/v1/coins/${coinId}`);
                 const infoData = infoResponse.data;
-                console.log(infoData);
                 
                 const priceResponse = await axios.get(`https://api.coinpaprika.com/v1/tickers/${coinId}`);
                 const priceData = priceResponse.data;
-                console.log(priceData);
                 setInfo(infoData);
                 setPriceInfo(priceData);
             } catch (error) {
