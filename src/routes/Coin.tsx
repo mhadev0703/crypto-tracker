633b11ca35d071f2958fab762a203a4d8c92044e
하민철 <minchulha0703@gmail.com>
2024-03-12 17:52:33 -0400

    Coin info page UI implementation

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -61,6 +61,7 @@ interface LocationState {
     }
 }
 
+
 interface InfoData {
     id: string;
     name: string;
@@ -70,8 +71,6 @@ interface InfoData {
     is_active: boolean;
     type: string;
     logo: string;
-    tags: string;
-    team: string[];
     description: string;
     message: string;
     open_source: boolean;
@@ -81,9 +80,6 @@ interface InfoData {
     proof_type: string;
     org_structure: string;
     hash_algorithm: string;
-    links: any;
-    links_extended: any;
-    whitepaper: any;
     first_data_at: string;
     last_data_at: string;
 }
@@ -97,28 +93,28 @@ interface PriceData {
     max_supply: number;
     beta_value: number;
     first_data_at: string;
-    last_updated: string;
+    last_updated: string; 
     quotes: {
         USD: {
-          ath_date: string;
-          ath_price: number;
-          market_cap: number;
-          market_cap_change_24h: number;
-          percent_change_1h: number;
-          percent_change_1y: number;
-          percent_change_6h: number;
-          percent_change_7d: number;
-          percent_change_12h: number;
-          percent_change_15m: number;
-          percent_change_24h: number;
-          percent_change_30d: number;
-          percent_change_30m: number;
-          percent_from_price_ath: number;
-          price: number;
-          volume_24h: number;
-          volume_24h_change_24h: number;
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
         };
-    }
+    };
 }
 
 function Coin() {
@@ -136,8 +132,10 @@ function Coin() {
                 
                 const priceResponse = await axios.get(`https://api.coinpaprika.com/v1/tickers/${coinId}`);
                 const priceData = priceResponse.data;
+
                 setInfo(infoData);
                 setPriceInfo(priceData);
+                setLoading(false);
             } catch (error) {
                 console.error("Error fetching data:", error);
             } finally {
@@ -154,7 +152,37 @@ function Coin() {
                     {state?.name ? state.name : loading ? "Loading..." : info?.name}
                 </Title>
             </Header>
-            {loading ? (<Loader>Loading...</Loader>) : null}
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Supply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                </>
+            )}
         </Container> 
     );
 }
