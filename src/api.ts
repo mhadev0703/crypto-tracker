d2f6a1e6f0cd71ddc369a559132257db548107d9
mhadev0703 <minchulha0703@gmail.com>
2024-03-22 23:55:43 -0400

    Add fetch functions from Coin.tsx for react query environment

--- src/api.ts
+++ src/api.ts
@@ -8,9 +8,22 @@ export interface ICoin {
     type: string,
 }
 
+const BASE_URL = `https://api.coinpaprika.com/v1`
+
 export async function fetchCoins() {
     const coins: ICoin[] = await fetch (
-        "https://api.coinpaprika.com/v1/coins"
-    ).then((response) => response.json());
+        `${BASE_URL}/coins`).then((response) => response.json());
+    return coins.slice(0, 100);
+}
+
+export async function fetchCoinInfo(coinId: string) {
+    const coins: ICoin[] = await fetch (
+        `${BASE_URL}/coins/${coinId}`).then((response) => response.json());
+    return coins.slice(0, 100);
+}
+
+export async function fetchCoinTickers(coinId: string) {
+    const coins: ICoin[] = await fetch (
+        `${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
     return coins.slice(0, 100);
 }
