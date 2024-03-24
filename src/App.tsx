74ecb413a26367b654702394bdc99e15fc6089b8
mhadev0703 <minchulha0703@gmail.com>
2024-03-23 23:57:58 -0400

    Install and import React Query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Router from "./Router";
 import { createGlobalStyle } from "styled-components";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap')
@@ -72,6 +73,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
