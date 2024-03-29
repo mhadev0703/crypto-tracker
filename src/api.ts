345009748616a4fa9fc05891c49c338df9e6b510
mhadev0703 <minchulha0703@gmail.com>
2024-03-28 23:42:18 -0400

    Add coin history data fetch and initial setup with Apexchart

--- src/api.ts
+++ src/api.ts
@@ -27,3 +27,9 @@ export async function fetchCoinTickers(coinId: string) {
       response.json()
     );
 }
+
+export async function fetchCoinHistory(coinId: string) {
+    return fetch(
+      `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+    ).then((response) => response.json());
+}
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,54 @@
-function Chart() {
-    return <h1>Chart</h1>
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+interface ChartProps {
+    coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+    const { isLoading, data } = useQuery<IHistorical[]>({
+        queryKey: ["ohlcv", coinId], 
+        queryFn: () => fetchCoinHistory(coinId),
+    });
+    
+    return (
+        <div>
+            {isLoading ? (
+                "Loading chart..."
+            ) : (
+                <ApexChart 
+                    type="line" 
+                    series={[
+                        {   
+                            name: "sales",
+                            data: data?.map((price) => price.close) ?? [],
+                        },
+                    ]}
+                    options={{
+                        theme: {
+                            mode: "dark",
+                        },
+                        chart: {
+                            height: 500,
+                            width: 500,
+                        },
+                    }}
+                />
+            )}
+        </div>
+    );
 }
 
 export default Chart;
