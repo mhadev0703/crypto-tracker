05aa47ac1c788797dd6f4f3d807dea91257ad519
mhadev0703 <minchulha0703@gmail.com>
2024-05-13 23:58:42 -0400

    Add data fetch for price page

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,4 +1,27 @@
-function Price() {
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+
+interface IHistorical {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+interface PriceProps {
+    coinId: string;
+}
+
+function Price({ coinId }: PriceProps) {
+    const { isLoading, data } = useQuery<IHistorical[]>({
+        queryKey: ["ohlcv", coinId], 
+        queryFn: () => fetchCoinHistory(coinId),
+    });
+    
     return <h1>Price</h1>
 }
 
