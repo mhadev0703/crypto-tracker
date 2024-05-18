2721fd686c0a919a8a9d0e7febe3cf4fd2f43108
mhadev0703 <minchulha0703@gmail.com>
2024-05-17 23:13:11 -0400

    Modify api.ts to be compatible with CoinGecko's API

--- src/api.ts
+++ src/api.ts
@@ -1,58 +1,22 @@
-export interface ICoin {
-    id: string,
-    name: string,
-    symbol: string,
-    rank: number,
-    is_new: boolean,
-    is_active: boolean,
-    type: string,
+const BASE_URL = `https://api.coingecko.com/api/v3/`;
+
+async function fetchWithCredentials(url: string) {
+  return fetch(url, {
+    credentials: 'same-origin',
+  }).then((response) => response.json());
 }
 
-{/*
-const BASE_URL = `https://api.coinpaprika.com/v1`
-
 export async function fetchCoins() {
-    const coins: ICoin[] = await fetch (
-        `${BASE_URL}/coins`).then((response) => response.json());
-    return coins.slice(0, 100);
+  const url = `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1`;
+  return fetchWithCredentials(url);
 }
 
 export async function fetchCoinInfo(coinId: string) {
-    return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
-      response.json()
-    );
-}
- 
-export async function fetchCoinTickers(coinId: string) {
-    return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
-      response.json()
-    );
+  const url = `${BASE_URL}/coins/${coinId}?localization=false`;
+  return fetchWithCredentials(url);
 }
 
 export async function fetchCoinHistory(coinId: string) {
-    return fetch(
-      `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
-    ).then((response) => response.json());
-}
-*/}
-
-const BASE_URL = `https://api.coingecko.com/api/v3`
-const days = 14
-
-export async function fetchCoins() {
-    const coins: ICoin[] = await fetch (
-      `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1`).then((response) => response.json());
-    return coins.slice(0, 50);
-}
-
-export async function fetchCoinInfo(coinId: string) {
-    return fetch(
-        `${BASE_URL}/coins/${coinId}?localization=false`
-      ).then((response) => response.json());
-}
-
-export async function fetchCoinHistory(coinId: string) {
-    return fetch(
-        `${BASE_URL}/coins/${coinId}/ohlc?vs_currency=usd&days=${days}`
-      ).then((response) => response.json());
+  const url = `${BASE_URL}/coins/${coinId}/ohlc?vs_currency=usd&days=14`;
+  return fetchWithCredentials(url);
 }
