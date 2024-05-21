5c7848c07a604904c815ded2de51344547e46055
mhadev0703 <minchulha0703@gmail.com>
2024-05-20 23:52:06 -0400

    Initial setup for dark and light mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { theme } from "./theme";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient()
@@ -12,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
         <App />
-      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
