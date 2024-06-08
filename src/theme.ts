67578696f025360377414be9c55930f55d8cce2d
mhadev0703 <minchulha0703@gmail.com>
2024-06-07 23:57:19 -0400

    Add toggle and icon color

--- src/theme.ts
+++ src/theme.ts
@@ -2,12 +2,18 @@ import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
     bgColor: "#2f3640",
-    textColor: "#f5f6fa",
+    nameColor: "black", 
+    textColor: "whitesmoke",
     accentColor: "#9c88ff",
+    toggleColor: "whitesmoke",
+    iconColor: "#fDB813",
 };
-
-export const lightTheme: DefaultTheme = {
+  
+  export const lightTheme: DefaultTheme = {
     bgColor: "whitesmoke",
+    nameColor: "black",
     textColor: "black",
     accentColor: "#9c88ff",
+    toggleColor: "#2f3640",
+    iconColor: "#f0c420",
 };
